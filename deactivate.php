cc60b8f8722f817d976a8e04f4a52c4b778eb4a9
jk <jk@10.0.0.4>
2015-06-11 15:06:04 +0600

    add index photo widget to mobile version

--- deactivate.php
+++ deactivate.php
@@ -33,3 +33,5 @@ OW::getNavigation()->deleteMenuItem('photo', 'mobile_photo');
 
 BOL_ComponentAdminService::getInstance()->deleteWidget('PHOTO_CMP_PhotoListWidget');
 BOL_ComponentAdminService::getInstance()->deleteWidget('PHOTO_CMP_UserPhotoAlbumsWidget');
+BOL_ComponentAdminService::getInstance()->deleteWidget('PHOTO_MCMP_PhotoListWidget');
+
