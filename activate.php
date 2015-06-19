c006ed3f3ae1fbeb55f099eeb10b18d22db5fb10
jk <jk@10.0.0.4>
2015-06-19 23:05:27 +0600

    fix mobile photo widget

--- activate.php
+++ activate.php
@@ -59,7 +59,7 @@ try
 {
     $widget = $widgetService->addWidget('PHOTO_MCMP_PhotoListWidget', false);
     $placeWidget = $widgetService->addWidgetToPlace($widget, BOL_MobileWidgetService::PLACE_MOBILE_INDEX);
-    $widgetService->addWidgetToPosition($placeWidget, BOL_MobileWidgetService::SECTION_TOP);
+    $widgetService->addWidgetToPosition($placeWidget, BOL_MobileWidgetService::SECTION_MOBILE_MAIN);
 }
 catch ( Exception $e )
 {
